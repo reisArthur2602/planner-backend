9b94efe494a1d851ffac7caa6f0cdfa3a1159218
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-03 13:34:39 -0300

    feat: add express tsx typescript

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on PORT ${process.env.PORT}`)
+);
