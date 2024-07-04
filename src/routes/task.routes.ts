f006139bbb77c52e68b04284c64ef8bee28e898d
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-04 11:02:17 -0300

    feat: add validacao do when nas rotas

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { TaskUseCase } from '../usecases/task.usecase';
 import { BadRequestError } from '../helpers/error';
 import { isAuthenticated } from '../middlewares/isAuthenticated';
+import { dateRegex } from '../utils/regex';
 
 export const TaskRoutes = Router();
 const taskUseCase = new TaskUseCase();
@@ -12,9 +13,19 @@ TaskRoutes.post('/', isAuthenticated, async (req, res) => {
   if (!title || !description || !when)
     throw new BadRequestError('Preencha os dados corretamente');
 
+  if (!dateRegex.test(when))
+    throw new BadRequestError(
+      'Parece que a data inserida é inválida. Por favor, verifique e tente novamente.'
+    );
+
   const user_id = req.userId;
 
-  await taskUseCase.create({ title, description, when, user_id });
+  await taskUseCase.create({
+    title,
+    description,
+    when: new Date(when),
+    user_id,
+  });
 
   return res.status(201).send();
 });
@@ -33,4 +44,22 @@ TaskRoutes.delete('/:id', isAuthenticated, async (req, res) => {
   await taskUseCase.delete({ id });
 
   return res.status(201).send();
+});
+
+TaskRoutes.put('/:id', isAuthenticated, async (req, res) => {
+  const { id } = req.params;
+
+  const { title, description, when } = req.body;
+
+  if (!title || !description || !when)
+    throw new BadRequestError('Preencha os dados corretamente');
+
+  if (!dateRegex.test(when))
+    throw new BadRequestError(
+      'Parece que a data inserida é inválida. Por favor, verifique e tente novamente.'
+    );
+
+  await taskUseCase.update({ id, title, description, when: new Date(when) });
+
+  return res.status(200).send();
 });
---
+++ src/utils/regex.ts
@@ -0,0 +1,1 @@
+export const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$/;
