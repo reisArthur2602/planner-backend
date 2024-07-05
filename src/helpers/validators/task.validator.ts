123979226af643042197142692ab648b02d873f6
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-05 05:26:14 -0300

    feat: add type ao validador

--- src/helpers/validators/task.validator.ts
+++ src/helpers/validators/task.validator.ts
@@ -14,13 +14,15 @@ export const TaskValidator = z.object({
       1,
       'Parece que a descrição da tarefa não foi enviada. Por favor, adicione uma descrição para continuar.'
     ),
+  type: z.enum(['study', 'gym', 'work', 'food', 'personal', 'travel'], {
+    message:
+      'O tipo de tarefa selecionado não é válido. Por favor, escolha um dos seguintes tipos: study, gym, work, food, personal, travel.',
+  }),
   when: z
     .string()
     .refine(
       (date) => dateRegex.test(date),
-      'O formato da data que você inseriu não é válido. Por favor, certifique-se de enviar a data no formato correto (por exemplo, AAAA-MM-DDTHH:MM.sss) e tente novamente.'
+      'A data que você enviou não está em um formato válido. Por favor, certifique-se de enviar a data no formato correto (por exemplo, AAAA-MM-DDTHH:MM.sss) e tente novamente.'
     )
     .transform((date) => new Date(date)),
 });
-
-// type: z.enum(['study', 'gym', 'work', 'food', 'people', 'travel', 'code']),
