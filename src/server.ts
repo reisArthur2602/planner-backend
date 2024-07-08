7ed7c7112717b7738ab594515ed526ee9c36eeb3
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-08 10:16:56 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'express-async-errors';
 import express from 'express';
+import cors from "cors"
 import { hasError } from './middlewares/hasError';
 import { UserRoutes } from './routes/user.routes';
 import { TaskRoutes } from './routes/task.routes';
@@ -8,6 +9,7 @@ import { TaskRoutes } from './routes/task.routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/user', UserRoutes);
 app.use('/task', TaskRoutes);
 app.use(hasError);
